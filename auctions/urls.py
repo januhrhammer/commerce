156d34007a63bf99f463ccdec47be31b07d07057
januhrhammer <jan.uhrhammer@gmail.com>
2022-09-07 12:03:20 +0200

    first commit

---
+++ auctions/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout"),
+    path("register", views.register, name="register"),
+    path("categories", views.category_view, name="categories"),
+    path("new_listing", views.new_listing, name="new_listing"),
+    path("auction/<str:pk>", views.auction_view, name="auction"),
+  
+]
---
+++ auctions/views.py
@@ -0,0 +1,120 @@
+from django.contrib.auth import authenticate, login, logout
+from django.db import IntegrityError
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
+from django.forms import ModelForm
+from .models import User, Listing, Bid, Category, Comment, Watchlist
+
+
+def index(request):
+    active_listings = Listing.objects.all().filter(active=True)
+    return render(
+        request,
+        "auctions/index.html",
+        {"auctions": active_listings, "title": "Active Auctions"},
+    )
+
+
+def login_view(request):
+    if request.method == "POST":
+
+        # Attempt to sign user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(
+                request,
+                "auctions/login.html",
+                {"message": "Invalid username and/or password."},
+            )
+    else:
+        return render(request, "auctions/login.html")
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
+
+
+def register(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+
+        # Ensure password matches confirmation
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            return render(
+                request, "auctions/register.html", {"message": "Passwords must match."}
+            )
+
+        # Attempt to create new user
+        try:
+            user = User.objects.create_user(username, email, password)
+            user.save()
+        except IntegrityError:
+            return render(
+                request,
+                "auctions/register.html",
+                {"message": "Username already taken."},
+            )
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "auctions/register.html")
+
+# Renders the category overview and passes all categories to the html
+def category_view(request):
+    categories = Category.objects.all()
+    return render(request, "auctions/categories.html", {"categories": categories})
+
+
+# Tries to render the specific auction by pk/id, on exception renders an error page
+def auction_view(request, pk):
+    try:
+        auction = Listing.objects.get(pk=pk)
+    except:
+        return render(request, "auctions/no_auction.html")
+    
+    return render(request, "auctions/auction.html", {
+        "auction": auction,
+        "pk": pk
+    })
+
+
+# Defines Form for new auction listing
+class AuctionForm(ModelForm):
+    class Meta:
+        model = Listing
+        fields = [
+            "item_name",
+            "item_description",
+            "starting_price",
+            "image",
+            "image_url",
+            "category",
+        ]
+
+# Renders the new listing page with form. When form is submitted, the auction page gets opened
+def new_listing(request):
+    form = AuctionForm(request.POST)
+    if form.is_valid():
+        new_listing = form.save(commit=False) # Saving the form without passing it to the db, because...
+        new_listing.user = request.user # Current User needs to be added
+        new_listing.save()
+
+        url = reverse("auction", kwargs={"pk": new_listing.pk})
+        return HttpResponse(url)
+
+    else:
+        return render(request, "auctions/new_listing.html", {
+            "form": form
+        })
